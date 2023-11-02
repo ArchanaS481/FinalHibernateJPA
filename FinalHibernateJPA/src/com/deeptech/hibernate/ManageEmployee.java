f1757b585d7d93e2d1ff56ca4fce6c47a5e59325
Archana_gowda <108232501+ArchanaS481@users.noreply.github.com>
2023-11-02 18:22:52 +0530

    Update ManageEmployee.java

--- FinalHibernateJPA/src/com/deeptech/hibernate/ManageEmployee.java
+++ FinalHibernateJPA/src/com/deeptech/hibernate/ManageEmployee.java
@@ -11,78 +11,114 @@ import org.hibernate.query.Query;
 public class ManageEmployee {
 
 	private static SessionFactory sf;
-	
+
 	public static void main(String[] args) {
-	
-		try 
+		
+		try
 		{
-		  sf = new Configuration().configure().addAnnotatedClass(Employee.class).buildSessionFactory();	
+			sf = new Configuration().configure().addAnnotatedClass(Employee.class).buildSessionFactory();
 		}
 		catch(Exception e)
 		{
 			e.printStackTrace();
 		}
-
-		//Creating Objects
-		ManageEmployee em = new  ManageEmployee();
-		Integer emp1 = em.addEmployee("Anu","Siri","Developer",25000); 
-		Integer emp2 = em.addEmployee("Shankar","S","Admin",35000); 
-		Integer emp3 = em.addEmployee("Santhosh","S","Developer",40000); 
-		Integer emp4 = em.addEmployee("Gowda","MR","Developer",30000);
-		em.listEmployee();
 		
-		
+		// Creating Objects 
+		ManageEmployee ME = new ManageEmployee();
+		Integer emp1 = ME.addEmployee("Archana","S","Developer",25000);
+		Integer emp2 = ME.addEmployee("Sai","Ganesh","Admin",35000);
+		Integer emp3 = ME.addEmployee("Naveen","Kumar","Developer",35000);
+		Integer emp4 = ME.addEmployee("Lokesh","M","Exe",25000);
+		Integer emp5 = ME.addEmployee("Santhosh","S","Developer",65000);
+		//ME.listEmployee();
+		ME.updateEmployee(emp1,45000);
+		ME.listEmployee();
+		ME.deleteEmployee(emp5);
 	}
-	public void listEmployee() {
-		Session s = sf.openSession();
-		Transaction tx = null;
-		
-		try {
-			tx = s.beginTransaction();
-			Query<Employee> q = s.createQuery("form Employee");
-			List<Employee> e = q.list();
-			for(Employee emp:e)
-			{
-				System.out.println(emp.getFirstname()+"\t"+emp.getLastName()+"\t"+emp.getDesignation()+"\t"+emp.getSalary());
-			}
-			
-		}
-		catch(Exception e)
-		{
-			e.printStackTrace();
-		}
-
-	}
-	public Integer addEmployee(String fname,String lname,String designation,double sal) 
+	public void deleteEmployee(Integer emp)
 	{
 		Session s = sf.openSession();
 		Transaction tx = null;
-		Integer employeeId = null;
+		try
+		{
+			tx = s.beginTransaction();
+			Employee empdata = s.get(Employee.class, emp);
+			s.delete(empdata);
+			tx.commit();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public void updateEmployee(Integer e1,double sal)
+	{
+		Session s = sf.openSession();
+		Transaction tx = null;
+		try
+		{
+			
+			tx = s.beginTransaction();
+			Employee emp = s.get(Employee.class, e1);
+			emp.setSalary(sal);
+			s.update(emp);
+			tx.commit();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public void listEmployee()
+	{
+		Session s = sf.openSession();
+		Transaction tx = null;
+		
+		try
+		{
+			tx = s.beginTransaction();
+			Query q = s.createQuery("from Employee");
+			List<Employee> e = q.list();
+			for(Employee emp :e)
+			{
+				System.out.println(emp.getFirstname()+"\t"+emp.getLastName()+"\t"+emp.getDesignation()+"\t"+emp.getSalary());
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public Integer addEmployee(String fname,String lname,String des,double sal)
+	{
+		Session s = sf.openSession();
+		Transaction tx = null;
+		Integer employeeID=null;
 		try
 		{
 			tx = s.beginTransaction();
 			Employee emp = new Employee();
 			emp.setFirstname(fname);
 			emp.setLastName(lname);
-			emp.setDesignation(designation);
+			emp.setDesignation(des);
 			emp.setSalary(sal);
-			employeeId = (Integer)s.save(emp);
-			System.out.println("Employee Record is saved");
+			employeeID = (Integer) s.save(emp);
+			System.out.println("Employee Record is Saved");
 			tx.commit();
 			
 		}
-		catch(Exception e) 
+		catch(Exception e)
 		{
 			e.printStackTrace();
-
 		}
 		finally
 		{
 			s.close();
 			//sf.close();
 		}
-		return employeeId;
+		
+		return employeeID;
 	}
-	
-
 }
